27302aefe86c80bb1a4f7cf2b05d3e1f536210a4
Owen Kitchener <okitchener@my.wctc.edu>
2025-11-11 14:06:02 -0600

    [1.15a] Display products for select category

--- Program.cs
+++ Program.cs
@@ -15,6 +15,7 @@ do
 {
     Console.WriteLine("1) Display categories");
     Console.WriteLine("2) Add category");
+      Console.WriteLine("3) Display Category and related products");
     Console.WriteLine("Enter to quit");
     string? choice = Console.ReadLine();
     Console.Clear();
@@ -22,58 +23,74 @@ do
 
     if (choice == "1")
     {
-       var configuration = new ConfigurationBuilder()
-            .AddJsonFile($"appsettings.json");
+        var configuration = new ConfigurationBuilder()
+             .AddJsonFile($"appsettings.json");
 
-    var config = configuration.Build();
+        var config = configuration.Build();
 
-    var db = new DataContext();
-    var query = db.Categories.OrderBy(p => p.CategoryName);
+        var db = new DataContext();
+        var query = db.Categories.OrderBy(p => p.CategoryName);
 
-    Console.ForegroundColor = ConsoleColor.Green;
-    Console.WriteLine($"{query.Count()} records returned");
-    Console.ForegroundColor = ConsoleColor.Magenta;
-    foreach (var item in query)
-    {
-      Console.WriteLine($"{item.CategoryName} - {item.Description}");
-    }
-    Console.ForegroundColor = ConsoleColor.White;
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine($"{query.Count()} records returned");
+        Console.ForegroundColor = ConsoleColor.Magenta;
+        foreach (var item in query)
+        {
+            Console.WriteLine($"{item.CategoryName} - {item.Description}");
+        }
+        Console.ForegroundColor = ConsoleColor.White;
     }
     else if (choice == "2")
     {
         Category category = new();
-    Console.WriteLine("Enter Category Name:");
-    category.CategoryName = Console.ReadLine()!;
-    Console.WriteLine("Enter the Category Description:");
-    category.Description = Console.ReadLine();
-       ValidationContext context = new ValidationContext(category, null, null);
-    List<ValidationResult> results = new List<ValidationResult>();
+        Console.WriteLine("Enter Category Name:");
+        category.CategoryName = Console.ReadLine()!;
+        Console.WriteLine("Enter the Category Description:");
+        category.Description = Console.ReadLine();
+        ValidationContext context = new ValidationContext(category, null, null);
+        List<ValidationResult> results = new List<ValidationResult>();
 
-    var isValid = Validator.TryValidateObject(category, context, results, true);
-    if (isValid)
+        var isValid = Validator.TryValidateObject(category, context, results, true);
+        if (isValid)
+        {
+            var db = new DataContext();
+            // check for unique name
+            if (db.Categories.Any(c => c.CategoryName == category.CategoryName))
+            {
+                // generate validation error
+                isValid = false;
+                results.Add(new ValidationResult("Name exists", ["CategoryName"]));
+            }
+            else
+            {
+                logger.Info("Validation passed");
+                // TODO: save category to db
+            }
+        }
+        if (!isValid)
+        {
+            foreach (var result in results)
+            {
+                logger.Error($"{result.MemberNames.First()} : {result.ErrorMessage}");
+            }
+        }
+    }
+else if (choice == "3")
+  {
+    var db = new DataContext();
+    var query = db.Categories.OrderBy(p => p.CategoryId);
+
+    Console.WriteLine("Select the category whose products you want to display:");
+    Console.ForegroundColor = ConsoleColor.DarkRed;
+    foreach (var item in query)
     {
-       var db = new DataContext();
-      // check for unique name
-      if (db.Categories.Any(c => c.CategoryName == category.CategoryName))
-      {
-        // generate validation error
-        isValid = false;
-        results.Add(new ValidationResult("Name exists", ["CategoryName"]));
-      }
-      else
-      {
-        logger.Info("Validation passed");
-        // TODO: save category to db
-      }
+      Console.WriteLine($"{item.CategoryId}) {item.CategoryName}");
     }
-    if (!isValid)
-    {
-      foreach (var result in results)
-      {
-        logger.Error($"{result.MemberNames.First()} : {result.ErrorMessage}");
-      }
-    }
-    }
+    Console.ForegroundColor = ConsoleColor.White;
+    int id = int.Parse(Console.ReadLine()!);
+    Console.Clear();
+    logger.Info($"CategoryId {id} selected");
+  }
     else if (String.IsNullOrEmpty(choice))
     {
         break;
