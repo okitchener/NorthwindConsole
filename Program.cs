1aff92e4cea7aa4efd9c3e215efaa40f4bd5f7e5
Owen Kitchener <okitchener@my.wctc.edu>
2025-11-11 13:36:33 -0600

    [1.10] Display categories

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿using NLog;
 using System.Linq;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
 using NorthwindConsole.Model;
 string path = Directory.GetCurrentDirectory() + "//nlog.config";
 
@@ -19,7 +21,22 @@ do
 
     if (choice == "1")
     {
-        // display categories
+       var configuration = new ConfigurationBuilder()
+            .AddJsonFile($"appsettings.json");
+
+    var config = configuration.Build();
+
+    var db = new DataContext();
+    var query = db.Categories.OrderBy(p => p.CategoryName);
+
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine($"{query.Count()} records returned");
+    Console.ForegroundColor = ConsoleColor.Magenta;
+    foreach (var item in query)
+    {
+      Console.WriteLine($"{item.CategoryName} - {item.Description}");
+    }
+    Console.ForegroundColor = ConsoleColor.White;
     }
     else if (choice == "2")
     {
